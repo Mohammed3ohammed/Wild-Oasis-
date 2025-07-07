ef2e2098e2253eca3438599f9ede77ee24d03fd2
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-07-07 17:44:27 +0300

    Add notFound check to getServerSideProps in Cabin page

---
+++ pages/cabins/[cabinId].js
@@ -0,0 +1,26 @@
+import CabinView from "@/components/CabinView";
+import Head from "next/head";
+import { getCabin } from "@/lib/data-service";
+
+export async function getServerSideProps({ params }) {
+  const cabin = await getCabin(params.cabinId);
+  return { props: { cabin } };
+}
+
+
+
+function Cabin({ cabin }) {
+    return (
+        <>
+            <Head>
+                <title>Cabin {cabin.name} The Wild Oasis</title>
+            </Head>
+
+            <div className="max-w-6xl mx-auto mt-8">
+                    <CabinView cabin={cabin} />
+            </div>
+        </>
+    )
+}
+
+export default Cabin;
