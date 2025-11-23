56e62b46055951d985e500b73ff19acfba65150b
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-11-23 22:55:40 +0200

    feat(Header): add user session image and Guest area link

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,7 +1,11 @@
 import Link from "next/link";
+import { auth } from "../lib/"
+import Image from "next/image";
 
+export default async function Navigation() {
 
-export default function Navigation() {
+    const session = await auth();
+
     return (
         <nav className="z-10 text-xl">
             <ul className="flex gap-16 items-center">
@@ -22,6 +26,31 @@ export default function Navigation() {
                     >
                         About
                     </Link>
+                </li>
+                <li>
+                    {
+                        session?.user?.image ? (
+                            <Link 
+                            href="/account"
+                            className="hover:text-accent-400 transition-colors flex items-center gap-4"
+                            >
+
+                                        <Image
+                                            className="h-8 rounded-full"
+                                            src={session.user.image}
+                                            alt={session.user.name}
+                                            referrerPolicy="no-referrer"
+                                                        />
+                                        <span>Guest area</span>                
+                            </Link>
+                        ) : (
+                            <Link
+                                href="/account"
+                                className="hover:text-accent-400 transition-colors"
+                            >
+                                Guest area
+                            </Link>
+                        )}
                 </li>
                 
                 <li>
