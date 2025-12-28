2ac9cef006b3033de6c6afd9d223f4b769e2dad4
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-12-28 15:46:18 +0200

    Update

--- middleware.js
+++ middleware.js
@@ -6,13 +6,33 @@
 //   matcher: ["/account"],
 // };
 
-import { withAuth } from "next-auth/middleware";
+// import { withAuth } from "next-auth/middleware";
 
-export const middleware = withAuth({
-  secret: process.env.NEXTAUTH_SECRET, // فقط secret
-  pages: { signIn: "/login" },        // صفحة تسجيل الدخول
-});
+// export const middleware = withAuth({
+//   secret: process.env.NEXTAUTH_SECRET, // فقط secret
+//   pages: { signIn: "/login" },        // صفحة تسجيل الدخول
+// });
+
+// export const config = {
+//   matcher: ["/account", "/profile"],   // الصفحات المحمية
+// };
+
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req) {
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ["/account", "/profile"],   // الصفحات المحمية
+  matcher: ["/account/:path*"],
 };
